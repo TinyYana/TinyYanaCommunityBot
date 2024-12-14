c93ce0c5631da3745904268d0175dd464c3de6c3
夜花櫻 <yanasakuranight@gmail.com>
2024-12-14 15:33:45 +0800

    初步實作成員資料儲存

---
+++ src/models/CommunityMemberData.ts
@@ -0,0 +1,21 @@
+import { ICommunityMemberData } from "../interfaces/ICommunityMemberData";
+
+export class CommunityMemberData implements ICommunityMemberData {
+    public discordId: string;
+    public money: number;
+    public softDelete: boolean;
+
+    constructor(discordId: string, money: number, softDelete: boolean) {
+        this.discordId = discordId;
+        this.money = money;
+        this.softDelete = softDelete;
+    }
+
+    addMoney(amount: number): void {
+        this.money += amount;
+    }
+
+    removeMoney(amount: number): void {
+        this.money -= amount;
+    }
+}
