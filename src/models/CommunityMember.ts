d4c736f2b12645669a6f4467d7c2d037d8ff7016
夜花櫻 <yanasakuranight@gmail.com>
2024-12-15 21:55:38 +0800

    softDelete->isDeleted 及 新增setActive

--- src/models/CommunityMember.ts
+++ src/models/CommunityMember.ts
@@ -3,18 +3,21 @@ import { ICommunityMember } from "../interfaces/ICommunityMember";
 export class CommunityMember implements ICommunityMember {
     public discordId: string;
     public money: number;
-    public softDelete: boolean;
+    public isDeleted: boolean;
     public isActive: boolean;
-    constructor(discordId: string, money: number, softDelete: boolean, isActive: boolean) {
+    constructor(discordId: string, money: number, isDeleted: boolean, isActive: boolean) {
         this.discordId = discordId;
         this.money = money;
-        this.softDelete = softDelete;
         this.isActive = isActive;
-        this.softDelete = softDelete;
+        this.isDeleted = isDeleted;
     }
 
     setDelete(status: boolean): void {
-        this.softDelete = status;
+        this.isDeleted = status;
+    }
+
+    setActive(status: boolean): void {
+        this.isActive = status;
     }
 
     addMoney(amount: number): void {
