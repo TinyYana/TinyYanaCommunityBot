e474a4255c7178b50e8752b5248714e1a7c5f9c0
夜花櫻 <yanasakuranight@gmail.com>
2024-12-19 20:43:02 +0800

    remove datamanger interface

--- src/interfaces/IDataManager.ts
+++
@@ -1,7 +0,0 @@
-export interface IDataManager<T> {
-    load(): Promise<T[]>; // 載入資料
-    save(data: T[]): Promise<void>; // 儲存資料
-    add(item: T): Promise<void>; // 新增單個項目
-    update(item: T): Promise<void>; // 更新單個項目
-    delete(id: string): Promise<void>; // 刪除單個項目
-}
