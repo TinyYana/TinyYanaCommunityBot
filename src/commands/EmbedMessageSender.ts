f44b54c8be9bf885906fe3f00e11f31e3137cb11
TinyYana <yanasakuranight@gmail.com>
2025-01-22 12:58:55 +0800

    add constructor and slashcommand

--- src/commands/EmbedMessageSender.ts
+++ src/commands/EmbedMessageSender.ts
@@ -1,4 +1,16 @@
+import { Client, SlashCommandBuilder } from "discord.js";
+import { logger } from "../utils/YanaUtil";
+
 class EmbedMessageSender {
+    constructor(client: Client) {
+        client.once('ready', () => {
+            const builder = new SlashCommandBuilder();
+            builder
+                .setName('embed')
+                .setDescription('Send an embed message to Discord.');
+            client.application?.commands.create(builder);
+        });
+    }
     public sendEmbedMessage(message: string): void {
         // Send message to Discord
     }
